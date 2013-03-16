af5bfd6b106e929d0f5684f48e3664babd4ab1a6
freeze2ice <freeze2ice@gmail.com>
2013-03-16 13:43:22 +0100

    req-json-lastfm class tidy up

--- req-json-lastfm.js
+++ req-json-lastfm.js
@@ -10,6 +10,11 @@ var request = require('request');
 //used for inspect method to go through lasfm geo.getEvent API JSON tree
 //var util = require('util');
 
+
+//TODO:
+//here goes an JSON file that contains names of cities that program would fetch info for
+//here goes a for loop to gather info for every city in CITIESJSON
+
 function getAttr(url, callback){
     var parsedRslts ='';
     var totalPages = 0;
@@ -109,7 +114,6 @@ function getConcerts(url, page){
     //};
 };
 
-/*
 var url1 = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&location=paris&limit=100&api_key=dbc287366d92998e7f5fb5ba6fb7e7f1&format=json';
 
 request(url1, function(err, res, results){
@@ -152,145 +156,3 @@ request(url1, function(err, res, results){
         url = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&location=paris&limit=100&api_key=dbc287366d92998e7f5fb5ba6fb7e7f1&format=json&page=';
         //console.log(url);
         getAttr(url, getConcerts);
-/*
-request(url, function(err, res, results) {
-    
-    //chunk to store info captured from LastFm
-    //var chunk= '';
-    //var to store parsed chunk info
-    //var parsedJSON = '';
-    //var to store each parsed selection (object) from captured info
-    //var myobject = '';
-    //for testing stage
-    //storing all useful information on concerts HTML DATA
-    //var parsedChunk = '';
-    //storing concert information on jsonObj
-    //var jsonObj = {};
-    //events = {};
-    //events.event = [];
-    
-    //creating output file to save json
-    var outputFileName = './concert.json';
-    //variable for storing stringify JSON
-    //var jsonStringify = '';
-    
-    if(!err){
-        //console.log(results);
-        //chunk += results;
-        parsedJSON = JSON.parse(results);
-        //console.log('parsedJSON: ' + parsedJSON);
-        
-        pushEvents(parsedJSON, outputFileName, writeJsonToFile);
-    */   
-    /*    
-        var legnth =0;
-        length = (parsedJSON.events.event).length;
-        //console.log('length: ' + length);
-        
-        //creating JSON
-        for(i =0; i<length; i++){
-        myobject = parsedJSON.events.event[i];
-     */   
-    /*
-        console.log("-------------");
-        console.log('id: ' + myobject.id);
-        console.log('title: ' + myobject.title);
-        console.log('artists: ' + myobject.artists.artist);
-        console.log('address: ' + myobject.venue.name + '\n' + myobject.venue.location.street + '\n' + myobject.venue.location.postalcode + ' ' + myobject.venue.location.city + ' ' + myobject.venue.location.country);
-        console.log('latitude: ' + myobject.venue.location['geo:point']['geo:lat'] +' longitude: '+ myobject.venue.location['geo:point']['geo:long']);
-        console.log('URL: ' + myobject.url);
-        console.log('Date and Time: ' + myobject.startDate);
-    */
-     
-    /*   
-        //for testing stage
-        //Data to be shown in html page   
-        parsedChunk += '<hr> Number: ' + i + '<hr> ID: ' + myobject.id + '<hr>Title: ' + myobject.title + '<br> Artists: ' + myobject.artists.artist;
-        parsedChunk += '<hr> Address: ' + myobject.venue.name + '<br>' + myobject.venue.location.street + '<br>' + myobject.venue.location.postalcode;
-        parsedChunk += ' ' + myobject.venue.location.city + ' ' + myobject.venue.location.country;
-        parsedChunk += '<hr>Latitude: ' + myobject.venue.location['geo:point']['geo:lat'] +'<br> Longitude: '+ myobject.venue.location['geo:point']['geo:long'];
-        parsedChunk += '<hr>URL: ' + myobject.url + '<hr>Date and Time: ' + myobject.startDate;
-        //console.log('parsedchunk: ' + parsedChunk);
-            if(myobject.website != ''){
-            //console.log('WebSite: ' + myobject.website);
-            parsedChunk += '<hr>WebSite: ' + myobject.website + '<hr>';
-            //jsonObj.website = myobject.website;
-            //jsonObj.events.event.push({ website : myobject.website }); //pushes website out of events!
-        }
-        else{
-            //console.log('Website: none provided');
-            parsedChunk += '<hr>WebSite: none provided' + '<hr>';
-            //jsonObj.website = 'none provided';
-            //jsonObj.events.event.push({ website : 'none provided' }); //pushes website out of events!
-        }
-    */
-        
-    /*
-        //adding data to JSON
-        events.event.push({
-            id : myobject.id,
-            title : myobject.title,
-        artist : myobject.artists.artist,
-        address : {name: myobject.venue.name, street : myobject.venue.location.street, postalcode : myobject.venue.location.postalcode,
-                    city : myobject.venue.location.city, country : myobject.venue.location.country,
-                    'geo:point' : { 'geo:lat' : myobject.venue.location['geo:point']['geo:lat'], 'geo:long' : myobject.venue.location['geo:point']['geo:long']}},
-                    url : myobject.url,
-                    startDate : myobject.startDate,
-                    website : myobject.website
-        });
-     */   
-    /*
-        jsonObj.id = myobject.id;
-        jsonObj.title =  myobject.title;
-        jsonObj.artist = myobject.artists.artist;
-        jsonObj.address = {};
-        jsonObj.address.name = myobject.venue.name;
-        jsonObj.address.street = myobject.venue.location.street;
-        jsonObj.address.postalcode = myobject.venue.location.postalcode;
-        jsonObj.address.city = myobject.venue.location.city;
-        jsonObj.address.country = myobject.venue.location.country;
-        jsonObj.address['geo:point'] = {};
-        jsonObj.address['geo:point']['geo:lat'] = myobject.venue.location['geo:point']['geo:lat'];
-        jsonObj.address['geo:point']['geo:long'] = myobject.venue.location['geo:point']['geo:long'];
-        jsonObj.url = myobject.url;
-        jsonObj.startDate = myobject.startDate;
-    */
-    /*
-        //events contains some information in json (non-stringify-d)
-        //jsonStringify stores stringify-d events
-        jsonStringify = JSON.stringify(events, null, 4);
-        //console.log(jsonStringify);
-    */    
-    //    };
-    /*                
-        //writing them into file
-        fs.writeFile(outputFileName, jsonStringify, function(err) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log("JSON saved to " + outputFileName);
-    }    
-        });
-    */      
-    /*
-        //exporting stringify-d events
-        exports.jsonStringify = jsonStringify;
-        //exporting concert information
-        exports.parsedChunk = parsedChunk;
-        //console.log('json req-json-lastfm: ' + jsonStringify);
-    */
-/*    }
-    else 
-    console.log('\n ***error occured in req-json-lastfm.js***');
-    
-        });*/
-/*
-    };
-    
-        }
-    else console.log('*** Error in request initiation! ***');
-    
-    //exports.totalPages = totalPages;
-    
-});*/
- 

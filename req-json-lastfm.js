ccaf843abe83f26e3a4830d5e5f3b072b8112ff4
freeze2ice <freeze2ice@gmail.com>
2013-03-16 20:56:57 +0100

    req-json-lastfm: iterate each city from liste-villes.json, gets all concerts and saves each city in concerts/ & clean

--- req-json-lastfm.js
+++ req-json-lastfm.js
@@ -1,34 +1,43 @@
 #!/usr/bin/env node
-
-var fs = require('fs');
-var request = require('request');
-
 //used for inspect method to go through lasfm geo.getEvent API JSON tree
 //var util = require('util');
+var fs = require('fs');
+var request = require('request');
+var villes = require('./liste-villes');
+console.log('*** Total number of cities to process= ' + villes.france.length + ' ***');
 
-//TODO
-//here goes an JSON file that contains names of cities that program would fetch info for
-//here goes a for loop to gather info for every city in CITIESJSON
+function iterateCities(url, cities, callback){
+    for(i=0; i<cities.france.length; i++){
+        url1 = url + '&location=' + cities.france[i] + ',france';
+        getAttr(url1, getConcerts);
+    };
+};
 
 function getAttr(url, callback){
     var parsedRslts ='';
     var totalPages = 0;
     var perPage = 0;
-    var page = 0;    
-    //var url1 = '';
+    var page = 0;
+    var location ='';
     request(url, function(err, res, results){
         //console.log(results);
         parsedRslts = JSON.parse(results);
-        totalPages = parsedRslts.events['@attr'].totalPages;
+        //console.log('parsedrslts: ' + util.inspect(parsedRslts.error));
+        if(parsedRslts.error){
+            console.log('***error occured in getAttr***');
+        }
+        else {
+        total = parsedRslts.events['@attr'].total;
         perPage = parsedRslts.events['@attr'].perPage;
         page = parsedRslts.events['@attr'].page;
-        console.log("total pages to process: " + totalPages);
-        console.log("number of results per page: " + perPage);
-        
-        for(page=1; page<= totalPages; page++){
-            getConcerts(url, page);
+        location = parsedRslts.events['@attr'].location;
+        console.log('location: ' + location);
+        console.log("total concerts in this city to process: " + total);
+        //console.log("number of results per page: " + perPage);
+        //for(page=1; page<= total; page++){
+            getConcerts(url, total, location);
             //console.log("p= " + p + ' \n url= ' + url+p);
-        }
+        };
     });
 };
 
@@ -42,7 +51,7 @@ function writeJsonToFile(jsonStringify, outputFileName){
     });
 };
 
-function pushEvents(parsedJSON, outputFileName, callback){
+function pushEvents(parsedJSON, outputFileName, location, total, callback){
     var myobject = '';
     var jsonStringify = '';
     var events = {};
@@ -55,7 +64,7 @@ function pushEvents(parsedJSON, outputFileName, callback){
         myobject = parsedJSON.events.event[i];
     //adding data to JSON
     events.event.push({
-        id : myobject.id,
+        number : i+1,
         title : myobject.title,
         artist : myobject.artists.artist,
         address : {name: myobject.venue.name, street : myobject.venue.location.street, postalcode : myobject.venue.location.postalcode,
@@ -66,6 +75,7 @@ function pushEvents(parsedJSON, outputFileName, callback){
         website : myobject.website
         });
     };
+    events['@attr'] = {"location": location, "total" : total};
     //events contains some information in JSON (non-stringify-d)
         //jsonStringify stores stringify-d events
         jsonStringify = JSON.stringify(events, null, 4);
@@ -97,18 +107,17 @@ function htmlEvents(myobject){
         }
 };
 
-function getConcerts(url, page){
-    //for(i=1; i<=totalPages; i++){
-    request(url+page, function(err, res, results) {
+function getConcerts(url, limit, location){
+    var url2 = url + '&limit=' + limit;
+    console.log('url: ' +url2);
+    request(url2, function(err, res, results) {
         var parsedJSON = '';
-        var outputFileName = './concerts/concertsPage='+page+'.json';
+        var outputFileName = './concerts/('+location+')concerts.json';
         parsedJSON = JSON.parse(results);
         //console.log("results: " + util.inspect(parsedJSON));
-        pushEvents(parsedJSON, outputFileName, writeJsonToFile);
+        pushEvents(parsedJSON, outputFileName, location, limit, writeJsonToFile);
     });
-    //};
 };
 
-        url = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&location=paris&limit=100&api_key=dbc287366d92998e7f5fb5ba6fb7e7f1&format=json&page=';
-        //console.log(url);
-        getAttr(url, getConcerts);
+url = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&api_key=dbc287366d92998e7f5fb5ba6fb7e7f1&format=json';
+iterateCities(url, villes);

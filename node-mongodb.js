f8a1704b3a7653aaeab11e8d0e16e204bb827cf9
freeze2ice <freeze2ice@gmail.com>
2013-03-22 20:30:54 +0100

    Update: concert information is stored directly into database

--- node-mongodb.js
+++ node-mongodb.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 //Visual mongoDB
-var mongoExpress = require('mongo-express');
+//var mongoExpress = require('mongo-express');
 
 var Db = require('mongodb').Db;
 var Server = require('mongodb').Server;
@@ -9,34 +9,29 @@ var Server = require('mongodb').Server;
 //establishing new connection
 var client = new Db('test', new Server('127.0.0.1', 27017), {safe:false});
 
-//function to insert data into default collection
-var insertData = function(err, collection){
-    //collection.insert({name: 'something'});
-    //collection.insert(concert);
-    require("fs").readdirSync("./concerts").forEach(function(file, i) {
-            var test = [];
-            test[i] = require("./concerts/" + file);
-            collection.insert(test[i]);
-        });
-};
+var data ='';
 
-//function to list all data in default collection
-var listAllData = function(err, collection){
-    collection.find().toArray(function(err, results){
-        console.log(results);
-        client.close();
-    });
-};
+function getData(args) {
+    data = args;
+}
 
-//opening dataBase 
+function insertData(err, collection) {
+    collection.insert(data);
+    client.close();
+}
+
+function openClient(arg) {
+    getData(arg);
+    //opening dataBase 
 client.open(function(err,pClient){
     if(!err){
-        console.log('concert: ');
         //calling function insertData on collection 'test'
         client.collection("test", insertData);
-        console.log('fin d insert ');
-        //calling fucntion listAllDta on collection 'test'
-        //client.collection('test', listAllData);
+        console.log('data inserted successfully!');
+
     }
     else console.log('\n ***error occured in node-mongodb.js***');
 });
+}
+
+exports.openClient = openClient;
--- req-json-lastfm.js
+++ req-json-lastfm.js
@@ -1,33 +1,28 @@
 #!/usr/bin/env node
-//used for inspect method to go through lasfm geo.getEvent API JSON tree
-//var util = require('util');
+
 var fs = require('fs');
 var request = require('request');
 var villes = require('./liste-villes');
 console.log('*** Total number of cities to process= ' + villes.france.length + ' ***');
 
-function iterateCities(url, cities, callback){
+function iterateCities(url, cities){
     for(i=0; i<cities.france.length; i++){
         url1 = url + '&location=' + cities.france[i] + ',france';
-        getAttr(url1, getConcerts);
+        getAttr(url1);
     };
 }
 
-function getAttr(url, callback){
+function getAttr(url){
     var parsedRslts ='';
     var totalPages = 0;
-    var perPage = 0;
-    var page = 0;
     var location ='';
     request(url, function(err, res, results){
         parsedRslts = JSON.parse(results);
         if(parsedRslts.error){
-            console.log('***error occured in getAttr***');
+            console.log('\n***error could not fetch results for the this city!***\n');
         }
         else {
         total = parsedRslts.events['@attr'].total;
-        perPage = parsedRslts.events['@attr'].perPage;
-        page = parsedRslts.events['@attr'].page;
         location = parsedRslts.events['@attr'].location;
         console.log('location: ' + location);
         console.log("total concerts in this city to process: " + total);
@@ -45,9 +40,8 @@ function writeJsonToFile(jsonStringify, outputFileName){
     });
 }
 
-function pushEvents(parsedJSON, outputFileName, location, total, callback){
+function pushEvents(parsedJSON, location, total){
     var myobject = '';
-    var jsonStringify = '';
     var events = {};
     events.event = [];
     var legnth =0;
@@ -60,31 +54,37 @@ function pushEvents(parsedJSON, outputFileName, location, total, callback){
         number : i+1,
         title : myobject.title,
         artist : myobject.artists.artist,
-        address : {name: myobject.venue.name, street : myobject.venue.location.street, postalcode : myobject.venue.location.postalcode,
+        address : {name: myobject.venue.name, street : myobject.venue.location.street,
+                    postalcode : myobject.venue.location.postalcode,
                     city : myobject.venue.location.city, country : myobject.venue.location.country,
-                    'geo:point' : { 'geo:lat' : myobject.venue.location['geo:point']['geo:lat'], 'geo:long' : myobject.venue.location['geo:point']['geo:long']}},
+                    'geo:point' : { 'geo:lat' : myobject.venue.location['geo:point']['geo:lat'],
+                                    'geo:long' : myobject.venue.location['geo:point']['geo:long']}},
         url : myobject.url,
         startDate : myobject.startDate,
         website : myobject.website
         });
     };
     events['@attr'] = {"location": location, "total" : total};
-    //events contains some information in JSON (non-stringify-d)
-        //jsonStringify stores stringify-d events
-        jsonStringify = JSON.stringify(events, null, 4);
-        writeJsonToFile(jsonStringify, outputFileName);
+        callMongo(events, location);
 }
 
 function getConcerts(url, limit, location){
     var url2 = url + '&limit=' + limit;
-    console.log('url: ' +url2);
+    console.log('url: ' +url2 + '\n');
     request(url2, function(err, res, results) {
         var parsedJSON = '';
-        var outputFileName = './concerts/('+location+')concerts.json';
+        //var outputFileName = './concerts/('+location+')concerts.json';
         parsedJSON = JSON.parse(results);
-        pushEvents(parsedJSON, outputFileName, location, limit, writeJsonToFile);
+        pushEvents(parsedJSON, location, limit);
     });
 }
 
-url = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&api_key=dbc287366d92998e7f5fb5ba6fb7e7f1&format=json';
+function callMongo(data, location) {
+    var nodeGo = require('./node-mongodb');
+    console.log('processing database for: ' + location);
+    nodeGo.openClient(data);
+}
+
+var apiKey = 'dbc287366d92998e7f5fb5ba6fb7e7f1';
+var url = 'http://ws.audioscrobbler.com/2.0/?method=geo.getevents&api_key='+apiKey+'&format=json';
 iterateCities(url, villes);

7ef1f3286e1192ebe7538398cbd80e30d170c56a
freeze2ice <freeze2ice@gmail.com>
2013-04-27 19:15:21 +0200

    site indexing with google

--- js/tools.js
+++ js/tools.js
@@ -43,14 +43,3 @@ $( "#amount" ).val($( "#rayon" ).slider( "values", 0 ) + " Km");
         //get rayon value
         //rayon = $("#rayon").slider("values", 0);
 });
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-40498004-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-  

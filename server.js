ebdd125bc84fdfb08fbb15936490745e93079781
freeze2ice <freeze2ice@gmail.com>
2013-06-23 18:26:16 +0200

    minified js code

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.configure(function() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(app.router);
- // app.use(express.static(__dirname + '/../../public'));
 });
 
 // Passport session setup.
@@ -101,37 +100,11 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user, message: req.flash('error') });
 });
 
-// POST /login
-// Use passport.authenticate() as route middleware to authenticate the
-// request. If authentication fails, the user will be redirected back to the
-// login page. Otherwise, the primary route function function will be called,
-// which, in this example, will redirect the user to the home page.
-//
-// curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
 app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
   function(req, res) {
     res.redirect('/dashbord');
   });
-  
-// POST /login
-// This is an alternative implementation that uses a custom callback to
-// acheive the same functionality.
-/*
-app.post('/login', function(req, res, next) {
-passport.authenticate('local', function(err, user, info) {
-if (err) { return next(err) }
-if (!user) {
-req.flash('error', info.message);
-return res.redirect('/login')
-}
-req.logIn(user, function(err) {
-if (err) { return next(err); }
-return res.redirect('/users/' + user.username);
-});
-})(req, res, next);
-});
-*/
 
 app.get('/logout', function(req, res){
   req.logout();
@@ -142,7 +115,7 @@ app.listen(process.env.PORT || 3000);
 console.log('Server listening on port 3000 || ' + process.env.PORT);
 
 function startUpdate(fn){
-	var requestLastfm = require('./req-json-lastfm');
+	var requestLastfm = require('./req-json-lastfm.min');
 	requestLastfm.startUpdate();
 }
 

8235a7a05e0007b128dc3ee4000364c15cabf9b6
Hamed <freeze2ice@gmail.com>
2015-01-30 19:23:51 +0100

    moved into public/

---
+++ public/js/tools.js
@@ -0,0 +1,47 @@
+/*
+ * Address AutoComplete JQuery
+ */
+$(document).ready(function() {
+    //Init Geocoder
+    var geocoder = new google.maps.Geocoder();
+    //Autocomplete
+    $('input#address').autocomplete({
+        source : function(request, response) {
+            geocoder.geocode({
+                    'address' : $('input#address').val()
+            }, function(results, status) {
+                response($.map(results, function(item) {
+                    return {
+                        value : item.formatted_address
+                    }
+                }));
+            });
+        },
+        select : function(event, ui) {
+            $('input#address').val(ui.item['value']);
+        },
+        minLength : 2
+    });
+});
+
+/*
+ * Range Slider JQuery
+ */
+$(function() {
+$( "#range" ).slider({
+        range: "min",
+        orientation: "horizontal",
+        min: 5,
+        max: 500,
+        value: 10,
+        animate : "slow",
+        slide: function() {
+
+                $( "#amount" ).val($( "#range" ).slider( "value") + " km");
+                },
+        change: function() {
+                $( "#amount" ).val($( "#range" ).slider( "value") + " km");
+                }
+            });
+$( "#amount" ).val($( "#range" ).slider( "value") + " km");
+});

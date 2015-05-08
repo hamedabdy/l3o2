fe8f716202f1f5ab887f71646dd3dfff2ef8436e
Hamed <freeze2ice@gmail.com>
2015-05-08 14:47:36 +0200

    deploying to heroku

--- app/routes.js
+++ app/routes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
 				|| req.connection.socket.remoteAddress;
 			var ip_latitude   = 48.8588589
 				, ip_longitude  = 2.3470599;
-			ip = '46.194.140.155';
+			ip = '46.193.143.95';
 			console.log('\n\n[DEBUG] ip = ' + ip);
 			userlocation.getRemoteGeoLocationFromIp(ip, function(err, results){
 				var r = '';
@@ -29,9 +29,8 @@ module.exports = function(app) {
 						}
 					});
 				} else {
-					// console.log('results = ' + results);
 					r = JSON.parse(results);
-					getConcerts(r.latitude, r.longitude, 50, '', '', '', function(err, results) {
+					getConcerts(r.latitude, r.longitude, 100, '', '', '', function(err, results) {
 						if (!err) {
 							p = {lat : r.latitude, lng : r.longitude, concerts : results};
 							res.render('index', p);
@@ -48,6 +47,19 @@ module.exports = function(app) {
 				if (!err) {
 					p = {lat : p.lat, lng : p.lng, concerts : results};
 					res.render('index', p);
+				}
+			});
+		}
+	});
+
+	app.get('/m', function(req, res){
+		if(Object.keys(req.query).length != 0){
+			var lat = req.query.lat
+				, lng = req.query.lng
+				, artist = req.query.artist;
+			getConcerts(lat, lng, 10, artist, '', '', function(err, results){
+				if(!err){
+					res.render('map', {concerts : results});
 				}
 			});
 		}
@@ -83,10 +95,11 @@ module.exports = function(app) {
  * @param fn : callback function (err, results)
  */
 function getConcerts (lat, lng, radius, artists, limit, date, fn) {
+	console.log("lat = " + lat + " lng = " + lng + " radius = " + radius + " artist = " + artists);
  	var l = 5000;
  	if (limit) l = limit;
  	var dbQuery = { latlong: { $near:[parseFloat(lat), parseFloat(lng)], $maxDistance: parseFloat(radius)/111.12} };
- 	// if (artists) dbQuery.artist = new RegExp(artists, 'i');
+ 	if (artists) dbQuery.artist = new RegExp(artists, 'i');
  	var newDate = new Date();
  	if (date) newDate = date;
  	dbQuery.startDate = { $gte : new Date(newDate)};

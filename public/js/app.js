0580adb4374bf0a4a59ae7e2599825d8c96587a2
Hamed <freeze2ice@gmail.com>
2015-06-07 15:20:43 +0200

    Minimized number of .css and .js files. Adapted the new design to mobile devices Changed google analytics code - move to new CSS code cleanup and relative components Removed unused fonts and icons in fonts folder - added new fonts!

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,169 @@
 /*
+ * Google Analytics
+ */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-40498004-4', 'auto');
+ga('send', 'pageview');
+
+// ------------- CLASSIE.JS -----------
+
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ *
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+
+( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+window.classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+})( window );
+
+// -------------  TOOLS  --------------
+
+/*
+ * Address AutoComplete JQuery
+ */
+$(document).ready(function() {
+    //Init Geocoder
+    var geocoder = new google.maps.Geocoder();
+    //Autocomplete
+    $('input#address').autocomplete({
+        source : function(request, response) {
+            geocoder.geocode({
+                    'address' : $('input#address').val()
+            }, function(results, status) {
+                response($.map(results, function(item) {
+                    return {
+                        value : item.formatted_address
+                    }
+                }));
+            });
+        },
+        select : function(event, ui) {
+            $('input#address').val(ui.item['value']);
+        },
+        minLength : 2
+    });
+});
+
+/*
+ * Range Slider JQuery
+ */
+$( "#range" ).slider({
+        range: "min",
+        orientation: "horizontal",
+        min: 5,
+        max: 500,
+        value: 10,
+        animate : "slow",
+        slide: function() {
+                $( "#amount" ).val($( "#range" ).slider( "value" ) + " km");
+                },
+        change: function() {
+                $( "#amount" ).val($( "#range" ).slider( "value" ) + " km");
+                }
+            });
+$( "#amount" ).val($( "#range" ).slider( "value" ) + " km");
+
+/**
+ * Method to construct neccessary share buttons from given data(concert) and url
+ */
+function shareButtons (data, fn) {
+    var encodedURL = encodeURIComponent(document.URL+data.artist);
+    var image_252 = (data.image).replace("/64/", "/252/")
+        , summary = data.artist+' - '+new Date(data.startDate).toLocaleString()+' - '
+                    +data.address.name+', '+data.address.street + ', '
+                    +data.address.postalcode+', '+data.address.city+', '
+                    +data.address.country
+        , shareBtns = {};
+        shareBtns.fb_share = '<a href="https://www.facebook.com/sharer/sharer.php?s=100&p[url]='
+                    +encodedURL+'&p[title]='+data.title+'&p[summary]='
+                    +summary+'&p[images][0]='+image_252
+                    +'" target="_blank"><img width="25" src="images/fb_1.png" '
+                    +'alt="Share On Facebook" title="Share On Facebook"/></a>';
+        shareBtns.tw_share = '<a href="https://twitter.com/share?url='+encodedURL+'&text='+data.title
+                    +'+'+data.artist+'&via=ConcertDaCote&related=concertdacote,ConcertDaCote,'
+                    +'" target="_blank"><img width="25" src="images/twitter_1.png" '
+                    +'alt="Tweet" title="Tweet"/></a>';
+        shareBtns.lastfm = '<a target="_blank" href="'+data.url
+                    +'"><img width="25" src="images/lastfm.png" alt="More Info On Last.fm"'
+                    +'title="More Info On Last.fm"/></a>';
+        shareBtns.gplus = '<a href="https://plus.google.com/share?url=' + encodedURL
+                    +'" target="_blank"><img width="25" src="images/google_plus.png" '
+                    +'alt="Share on G+" title="Share On Google+"/></a>';
+        shareBtns.su = '<a href="http://stumbleupon.com/submit?url=' + encodedURL
+                    +'" target="_blank"><img width="25" src="images/stumble_upon.png" '
+                    +'alt="Stumble" title="Stumble"/></a>';
+        return fn(shareBtns);
+}
+
+// --------------------------
+
+/*
  * Side bar
  */
 var menuRight = document.getElementById( 'cbp-spmenu-s2' )

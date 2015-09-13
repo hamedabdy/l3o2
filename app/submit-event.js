2578f0371650c7a8212fe069340d2eed5f243f89
Hamed <freeze2ice@gmail.com>
2015-09-13 12:15:05 +0200

    encrypt score values to be unchangeable by user

--- app/submit-event.js
+++ app/submit-event.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 
 var mongojs     = require('mongojs')
-    ,configDB   = require('../config/database')
-    ,db         = mongojs(configDB.url, ['concerts'])
+    ,mongo      = require('./node-mongodb')
     ,util       = require('util')
-    ,logger     = require('../logger').appLog;
+    ,logger     = require('../logger').appLog
+    ,util       = require('util')
+    ,crypto     = require('crypto');
 
 module.exports = function(app) {
 
     app.get('/se', function(req, res){
         if(Object.keys(req.query).length == 0){
-            res.render('submit-event', {message: ''});
+            cipherArray([5, 50, 100, 150, 200, 400], function(arr){
+                var msg = req.session.msg;
+                req.session.msg = null;
+                res.render('submit-event', {message: msg, score : arr});
+            });
         } else {
             res.render('404', {});
         }
@@ -23,8 +28,38 @@ module.exports = function(app) {
         req.body.source = 'form';
         req.body.artist = req.body.artist.split(', ');
         req.body.tags = req.body.tags.split(', ');
-        req.body.score = parseInt(req.body.score);
-        db.concerts.insert(req.body);
-        res.render('submit-event', {message: 'Thank you for your submission'});
+        decipherText(req.body.score, function(decrypted){
+            req.body.score = parseInt(decrypted);
+            mongo.insertData(req.body);
+        });
+        // res.render('submit-event', {message: 'Thank you for your submission'});
+        req.session.msg = 'Thank you for your submission';
+        res.redirect('/se');
     });
+}
+
+
+/**
+ * Methods
+**/
+
+// Should eventually replace this by user cookie_id or user's hash
+var key = 'salt';
+
+function cipherArray (text_array, fn) {
+    arr = [];
+    for (var i = 0; i < text_array.length; i++) {
+        var cipher = crypto.createCipher('aes-256-cbc', key);
+        encrypted = cipher.update(String(text_array[i]), 'utf8', 'base64');
+        encrypted += cipher.final('base64');
+        arr.push(encrypted);
+    };
+    return fn(arr);
+}
+
+function decipherText (encrypted, fn) {
+    decipher = crypto.createDecipher('aes-256-cbc', key);
+    decrypted = decipher.update(encrypted, 'base64', 'utf8');
+    decrypted += decipher.final('utf8');
+    return fn(decrypted);
 }

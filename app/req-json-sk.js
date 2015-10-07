df285e46902b84be5d6cfbb6e52edc4587872384
Hamed <freeze2ice@gmail.com>
2015-10-07 11:31:22 +0200

    bug fix : removed address geocoder (limited). minor improvements

--- app/req-json-sk.js
+++ app/req-json-sk.js
@@ -17,8 +17,6 @@ var per_page = '&per_page='+MAX_PER_PAGE;
 var api_loc = 'http://api.songkick.com/api/3.0/search/locations.json?'+apiKey+per_page;
 var api_eve = 'http://api.songkick.com/api/3.0/events.json?'+apiKey+'&page=%s'+per_page;
 var img_cdn = 'http://images.sk-static.com/images/media/profile_images/artists/%s/card_avatar';
-var api_geo = 'http://open.mapquestapi.com/nominatim/v1/reverse.php?format=json&lat=%s&lon=%s';
-// var api_geo = 'http://nominatim.openstreetmap.org/reverse?format=json&lat=%s&lon=%s';
 
 console.log('\n*** Total number of locations to process : ' + pays.country.length + ' ***\n');
 console.log('*** Please be patient while sending requests this may take a while... ****\n');
@@ -58,8 +56,7 @@ function getEventAttr (url, city_name) {
             var r = (JSON.parse(results)).resultsPage;
             console.log('Fetching attributes for '+city_name+' with : '+options + ' -- totalEntries= '+r.totalEntries);
             if(r.totalEntries > 0){
-                // (r.totalEntries/MAX_PER_PAGE)+1 = total pages
-                var totalPages = parseInt(r.totalEntries/MAX_PER_PAGE)+1;
+                var totalPages = parseInt(r.totalEntries/MAX_PER_PAGE)+2;
                 for(p=1; p<totalPages; p++){
                     _url = util.format(url, p);
                     getEventPerCityId(_url, city_name);
@@ -75,20 +72,21 @@ function getEventPerCityId (url, city_name) {
     request(options, function(err, res, results){
         if(!err){
             var r = JSON.parse(results).resultsPage;
-            if(r.status=='ok'){
-                async.map(r.results.event, function eachItem (item, callback){
-                    var artist = [];
-                    (item.performance).forEach(function(a){
-                        artist.push(a.displayName);
-                    });
-                    reverseGeocode(item.location.lat, item.location.lng, function(err, result){
-                        if(!err){
+            parseJson(results, function(err, res){
+                if(!err){
+                    var r = res.resultsPage;
+                    if(r.status=='ok'){
+                        async.map(r.results.event, function eachItem (item, callback){
+                            var artist = [];
+                            (item.performance).forEach(function(a){
+                                artist.push(a.displayName);
+                            });
                             var eve = {
                                         '_id' : 'sk_'+item.id,
                                         'title' : item.displayName,
                                         'artist' : artist,
                                         'latlng' : [parseFloat(item.location.lat), parseFloat(item.location.lng)],
-                                        'address' : result,
+                                        'address' : "No address provided",
                                         'startDate' : ((!item.start.datetime) ?  new Date(item.start.date) : new Date(item.start.datetime)),
                                         'url' : item.uri,
                                         'img' : ((item.performance[0]) ? util.format(img_cdn, item.performance[0].artist.id) : null),
@@ -96,32 +94,25 @@ function getEventPerCityId (url, city_name) {
                                         'source' : 'songkick'
                                     };
                             callback(null, eve);
-                        }
-                    });
-                }, function done (err, result){
-                    console.log('Inserting events for -- ' + city_name + '--');
-                    database.insertData(result);
-                });
-            }
+                        }, function done (err, result){
+                            console.log('Inserting events for -- ' + city_name + '--');
+                            database.insertData(result);
+                        });
+                    }
+                }
+            });
         }
     });
 }
 
-
-function reverseGeocode (lat, lng, callback) {
-    url = util.format(api_geo, lat, lng);
-    request({uri: url}, function(err, res, result){
-        if(!err){
-            var r = null;
-            try{
-                r = JSON.parse(result);
-                return callback(null, r.house_number+', '+r.road+' '+r.postcode+', '+r.city+' '+r.country);
-            }
-            catch (e) {
-                return callback(e, null);
-            }
-        } else return callback(err, null);
-    });
+function parseJson(obj, fn){
+    var r = '';
+    try {
+        r = JSON.parse(obj);
+        return fn(null, r);
+    } catch (e) {
+        return fn(e, null);
+    }
 }
 
 function startUpdate(){

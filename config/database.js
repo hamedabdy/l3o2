303bf6a6248f3b636de0743488e7373fcae210cf
Hamed <freeze2ice@gmail.com>
2015-12-30 16:29:04 +0100

    DB moved to remote server (mongolab)

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 
+var prot = 'mongodb://';
+var db_user = 'concertdacote';
+var db_pass = 'Concertdacote789';
+var mongoLab_url = '@ds037155.mongolab.com:37155/concertdacote';
+var local_url = 'localhost/concertdacote';
+mongoLab_url = prot + db_user + ':' + db_pass + mongoLab_url;
+
 module.exports = {
-	'url' : process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/concertdacote'
+	'url' : process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || mongoLab_url
 };

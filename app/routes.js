2a89872f89bb56cb1dc6f8eef746d3e87d269df9
Hamed <freeze2ice@gmail.com>
2016-01-05 18:37:03 +0100

    code cleanup. Add getAddress from client side

--- app/routes.js
+++ app/routes.js
@@ -1,81 +1,82 @@
 #!/usr/bin/env node
 
-var mongojs       	= require('mongojs')
-	,configDB 		= require('../config/database')
-	,db 			= mongojs(configDB.url, ['concerts'])
-	,db2 			= mongojs(configDB.url, ['ads'])
-	,userlocation 	= require('./userlocation')
-	,util 			= require('util')
-	,logger			= require('../logger').appLog;
+var mongojs         = require('mongojs')
+    ,configDB       = require('../config/database')
+    ,db             = mongojs(configDB.url, ['concerts'])
+    ,db2            = mongojs(configDB.url, ['ads'])
+    ,userlocation   = require('./userlocation')
+    ,util           = require('util')
+    ,logger         = require('../logger').appLog;
 
 module.exports = function(app) {
 
-	app.get('/', function(req, res){
-		var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-		res.locals.url = fullUrl;
-		var o = { lat : 48.8588589, lng : 2.3470599, loc : 'unknown' };
-		if(Object.keys(req.query).length == 0) {
-			var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-			logger.debug('ip = ' + ip);
-			userlocation.getRemoteGeoLocationFromIp(ip, function(err, results){
-				var r = ""; 
-				r = (results) ? JSON.parse(results) : '{status:' + err +'}';
-				logger.debug(err + " " + r.status);
-				if(r.status == "success") {
+    app.get('/', function(req, res){
+        var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        res.locals.url = fullUrl;
+        var o = { lat : 48.8588589, lng : 2.3470599, loc : 'unknown' };
+        if(Object.keys(req.query).length == 0) {
+            var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+            logger.debug('ip = ' + ip);
+            userlocation.getRemoteGeoLocationFromIp(ip, function(err, results){
+                var r = ""; 
+                r = (results) ? JSON.parse(results) : '{status:' + err +'}';
+                logger.debug(err + " " + r.status);
+                if(r.status == "success") {
                     o = {lat : r.lat, lng : r.lon, loc : r.city + ', ' + r.country};
                     getConcerts(o.lat, o.lng, 100, '', 50, '', function(err, results) {
-						if (!err) {
-							o.concerts = results;
-							res.render('index', o);
-						}
-					});
-				} else {
-					logger.error(err);
-					getConcerts(o.lat, o.lng, 100, '', 50, '', function(err, results) {
-						if (!err) {
-							o.concerts = results;
-							res.render('index', o);
-						}
-					});
-				}
-			});
-		} else {
-				res.render('404', {});
-		}
-	});
+                        if (!err) {
+                            o.concerts = results;
+                            res.render('index', o);
+                        }
+                    });
+                } else {
+                    logger.error(err);
+                    getConcerts(o.lat, o.lng, 100, '', 50, '', function(err, results) {
+                        if (!err) {
+                            o.concerts = results;
+                            res.render('index', o);
+                        }
+                    });
+                }
+            });
+        } else {
+                res.render('404', {});
+        }
+    });
 
-	app.get('/m', function(req, res){
-		var q = req.query;
-		if(Object.keys(q).length != 0){
-			res.locals.query = q;
-			getConcerts(q.lat, q.lng, q.range, q.artist, '', '', function(err, results){
-				if(!err){
-					if (results.length == 1 || (q.artist && results.length>0)) {
-						results[0].score = parseInt(results[0].score)+1;
-						db.concerts.update({'_id': results[0]['_id']}, results[0]);
-						logger.trace(results[0]);
-					};
-					res.render('map', {concerts : results});
-				} else res.render('404', {});
-			});
-		}
-		else {
-			res.locals.query = null;
-			res.render('map', {concerts : []});
-		}
-	});
+    /**
+     * MAP route
+     * if a specifique request (address and artist name) the corresponding event's score is incremented
+     * in order to stimulate the click action!
+     */
+    app.get('/m', function(req, res){
+        var q = req.query;
+        if(Object.keys(q).length != 0){
+            res.locals.query = q;
+            if (q.artist)
+                db.concerts.findAndModify({ query: { artist: q.artist }, update: { $inc: { score: 1 } } });
+        }
+        else {
+            res.locals.query = null;
+            res.render('map', {concerts : []});
+        }
+    });
 
-	/*
-	 * Concerts API
-	 */
-	app.get('/concert', function(req, res){
-		var q =  req.query;
-		getConcerts(q.lat, q.long, q.range, q.artist, q.l, q.date, function(err, results) {
-			if (!err) {
-				res.send(results);
-			}
-		});
-	});
+    app.put('/location', function(req, res){
+        db.concerts.findAndModify({ query: { '_id' : req.body['_id'] }, update: { $set: { address: req.body.address } } });
+    });
+
+    /*
+     * Concerts API
+     */
+    app.get('/concert', function(req, res){
+        var q =  req.query;
+        getConcerts(q.lat, q.long, q.range, q.artist, q.l, q.date, function(err, results) {
+            if (!err) {
+                res.send(results);
+            }
+        });
+    });
 
 }
 
@@ -94,20 +95,20 @@ module.exports = function(app) {
  * @param fn : callback function (err, results)
  */
 function getConcerts (lat, lng, radius, artists, limit, date, fn) {
- 	var l = 500;
- 	if (limit) l = limit;
- 	var dbQuery = { latlng: { $near:[parseFloat(lat), parseFloat(lng)], $maxDistance: parseFloat(radius)/111.12} };
- 	if (artists) dbQuery.artist = new RegExp(artists, 'i');
- 	var newDate = new Date();
- 	if (date) newDate = date;
- 	dbQuery.startDate = { $gte : new Date(newDate)};
- 	db.concerts.find( dbQuery).sort({'score' : -1}).limit(l, function(err, result) {
-	   	if(!err) {
-    		logger.info('\n# of concerts returned = ' + result.length + '\n');
-	        return fn(null, result);
-	    } else {
-	    	logger.error(err);
-	    	return fn(err, null);
-	    }
-	});
+    var l = 500;
+    if (limit) l = limit;
+    var dbQuery = { latlng: { $near:[parseFloat(lat), parseFloat(lng)], $maxDistance: parseFloat(radius)/111.12} };
+    if (artists) dbQuery.artist = new RegExp(artists, 'i');
+    var newDate = new Date();
+    if (date) newDate = date;
+    dbQuery.startDate = { $gte : new Date(newDate)};
+    db.concerts.find( dbQuery).sort({'score' : -1}).limit(l, function(err, result) {
+        if(!err) {
+            logger.info('\n# of concerts returned = ' + result.length + '\n');
+            return fn(null, result);
+        } else {
+            logger.error(err);
+            return fn(err, null);
+        }
+    });
 }
